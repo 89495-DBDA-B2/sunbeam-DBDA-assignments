25d11718ddefb683995cb13d163daf69b8260cc6
python_codes <aniketkangude0727@gmail.com>
2025-02-26 15:59:49 +0530

    new folder

---
+++ daily_assignments/day3/pro3.py
@@ -0,0 +1,16 @@
+'''
+3] Write a program to accept a 4 digit number and
+   a. Display face value of each decimal digit
+   b. Display place value of each decimal digit
+   c. Display no in reverse order by changing decimal place values.
+   
+   If user enters a 4 digit number 9361 output should be
+   a. 9 3 6 1
+   b. 9361 = 9 000 + 300 + 60 + 9
+   c. 1639 
+   
+   '''
+   
+num=(input("Enter 4 Digit Number: "))
+for i in num:
+    print(i,end=" ")

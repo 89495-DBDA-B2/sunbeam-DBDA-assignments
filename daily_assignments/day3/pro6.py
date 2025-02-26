6be9213f1a0119052239af05f26a3ea8f1c1df51
python_codes <aniketkangude0727@gmail.com>
2025-02-26 23:18:25 +0530

    first edit

---
+++ daily_assignments/day3/pro6.py
@@ -0,0 +1,14 @@
+# 6] Write a Python Program Get age input from user and check if user is eligible for voting
+
+
+def function1(age):
+    if age >= 18:
+        return "You are eligible to vote. "
+    else:
+        return "You are not eligible to vote. "
+
+age = int(input("Enter your age: "))
+
+result = function1(age)
+
+print(result)

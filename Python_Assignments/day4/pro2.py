f9a73199f30bf1574f149c9e0b9a5382f3513108
Aniket Kangude <142888993+Aniket0727@users.noreply.github.com>
2025-02-27 23:14:34 +0530

    Delete Python_Assignments directory

--- Python_Assignments/day4/pro2.py
+++
@@ -1,7 +0,0 @@
-# 2) Write a program that accepts a list from user and print the alternate element of list.
-
-# Accept a list from the user
-user_list = input("Enter elements separated by spaces: ").split()
-
-# Print alternate elements using slicing
-print("Alternate elements:", user_list[::2])
--- Python_Assignments/day4/pro4.py
+++
@@ -1,9 +0,0 @@
-# 4) Write a program to find index of element ‘e’ in given vowels list [’a’, ’e’, ’i’,’o’, ’u’]
-
-volves=['a','e','i','o','u']
-
-
-index=volves.index('e')
-print('e is present at index: ',index)
-
-

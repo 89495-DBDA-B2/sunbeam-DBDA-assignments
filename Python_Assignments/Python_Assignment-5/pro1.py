138fd53cbbb029b9eaa5ace88fa90ee65233b186
aniket0727 <aniketkangude0727@gmail.com>
2025-03-04 18:50:18 +0530

    Python assignment 5 uploaded

---
+++ Python_Assignments/Python_Assignment-5/pro1.py
@@ -0,0 +1,14 @@
+# 1) Given a dictionary of students and their favourite colours:
+# people={'Arham':'Blue','Lisa':'Yellow',''Vinod:'Purple','Jenny':'Pink'}
+# A. Find out how many students are in the list
+# B. Change Lisa’s favourite colour
+# C. Remove 'Jenny' and her favourite colour
+# D. Sort and print students and their favourite colours alphabetically by name
+
+people={'Arham':'Blue','Lisa':'Yellow','Vinod':'Purple','Jenny':'Pink'}
+
+add=0
+for i in people.keys():
+    add=add+1
+
+print(f"{add} Students are available in list")
